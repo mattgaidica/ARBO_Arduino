2cd7e0991971edbb346f7274d7f255fdce1aab23
Matt Gaidica, Ph.D <matt@gaidi.ca>
2020-01-31 16:09:56 -0500

    Revert "Update validate_RTCaccuracy.ino"
    This reverts commit 896cb69eb8534e14e9eab206c1c6beb618461b6f.

--- Validation/validate_RTCaccuracy/validate_RTCaccuracy.ino
+++ Validation/validate_RTCaccuracy/validate_RTCaccuracy.ino
@@ -1,69 +1,94 @@
-#include <SPI.h>
-//#include <SD.h>
-#include "SdFat.h"
-SdFat sd;
+// can use https://www.time.gov/ to sync
 
-#define SPI_SPEED SD_SCK_MHZ(4)
-#define chipSelect 5 //4
-#define buttonPin 6
-#define ledPin 8
-#define FILE_BASE_NAME "BR_"
-SdFile file;
-const unsigned long period = 10000;
-unsigned long startMillis;
-int buttonState = 0;
+#include <RTCZero.h>
+
+#define GREEN 12 // Green LED on Pin #8
+/* Change these values to set the current initial time */
+byte hours = 0;
+byte minutes = 0;
+byte seconds = 0;
+/* Change these values to set the current initial date */
+const byte day = 11;
+const byte month = 10;
+const byte year = 19;
+
+RTCZero rtc;    // Create RTC object
+boolean clockSet = false;
 
 void setup() {
-  // Open serial communications and wait for port to open:
-  Serial.begin(9600);
-  pinMode(ledPin, OUTPUT); // initialize the LED pin as an output:
-  pinMode(buttonPin, INPUT_PULLUP);  // initialize the pushbutton pin as an input:
-  //  attachInterrupt(digitalPinToInterrupt(buttonPin), writeSD, CHANGE);
-  while (!Serial) {}
+  pinMode(13, OUTPUT);
+  digitalWrite(13, HIGH); // LIGHTS ON
+  
+  while (! Serial); // Wait until Serial is ready
+  Serial.begin(115200);
 
-  Serial.print("Initializing SD card...");
-  if (!sd.begin(chipSelect, SPI_SPEED)) {
-    Serial.println("initialization failed!");
-    return; // This return statement terminates the program.
+  Serial.println("Let's set the clock (H, M, S)");
+  while(clockSet == 0) {
+    if(hours == 0) {
+      while (Serial.available() > 0) {
+        hours = getSerialInt("hours");
+      }
+    } else if(minutes == 0) {
+      while (Serial.available() > 0) {
+        minutes = getSerialInt("minutes");
+      }
+    } else if(seconds == 0) {
+      while (Serial.available() > 0) {
+        seconds = getSerialInt("seconds");
+      }
+    } else {
+      clockSet = true;
+    }
   }
-  Serial.println("initialization done.");
+  Serial.println("Setting clock!");
+  rtc.begin();    // Start the RTC in 24hr mode
+  rtc.setTime(hours, minutes, seconds);   // Set the time
+  rtc.setDate(day, month, year);    // Set the date
+  SerialOutput();
+  digitalWrite(13, LOW); // LIGHTS OUT
 }
 
 void loop() {
-  if (!digitalRead(buttonPin)) {
-    writeSD();
+  blink(GREEN,5); // 1s
+  delay(1000);
+  SerialOutput();
+}
+
+// blink out an error code, Red on pin #13 or Green on pin #8
+void blink(uint8_t LED, uint8_t flashes) {
+  uint8_t i;
+  for (i=0; i<flashes; i++) {
+    digitalWrite(LED, HIGH);
+    delay(100);
+    digitalWrite(LED, LOW);
+    delay(100);
   }
 }
 
-void writeSD() {
-  //  detachInterrupt(digitalPinToInterrupt(buttonPin));
-  // Find an unused file name.
-  const uint8_t BASE_NAME_SIZE = sizeof(FILE_BASE_NAME) - 1;
-  char fileName[13] = FILE_BASE_NAME "00.txt";
-  while (sd.exists(fileName)) {
-    if (fileName[BASE_NAME_SIZE + 1] != '9') {
-      fileName[BASE_NAME_SIZE + 1]++;
-    } else if (fileName[BASE_NAME_SIZE] != '9') {
-      fileName[BASE_NAME_SIZE + 1] = '0';
-      fileName[BASE_NAME_SIZE]++;
-    }
-  }
-  Serial.println("ISR entered...");
-  startMillis = millis();
-  if (file.open(fileName, O_WRONLY | O_CREAT | O_EXCL)) {
-    digitalWrite(ledPin, HIGH);
-    while (millis() - startMillis < period) {
-      file.print("X");
-    }
-    file.close();
-    digitalWrite(ledPin, LOW);
-    Serial.println("done.");
-  }
-  // close the file:
-  else {
-    // if the file didn't open, print an error:
-    Serial.println("error opening file");
-  }
-  delay(1000); // switch debounce in case SD doesn't open
-  //  attachInterrupt(digitalPinToInterrupt(buttonPin), writeSD, CHANGE);
+byte getSerialInt(char *serialLabel) {
+  int val = Serial.parseInt();
+  byte serialInt = (byte)val;
+  Serial.print(serialLabel);
+  Serial.print(": ");
+  Serial.println(serialInt);
+  return serialInt;
+}
+
+// Debbugging output of time/date
+void SerialOutput() {
+  Serial.print(rtc.getDay());
+  Serial.print("/");
+  Serial.print(rtc.getMonth());
+  Serial.print("/");
+  Serial.print(rtc.getYear()+2000);
+  Serial.print(" ");
+  Serial.print(rtc.getHours());
+  Serial.print(":");
+  if(rtc.getMinutes() < 10)
+    Serial.print('0');      // Trick to add leading zero for formatting
+  Serial.print(rtc.getMinutes());
+  Serial.print(":");
+  if(rtc.getSeconds() < 10)
+    Serial.print('0');      // Trick to add leading zero for formatting
+  Serial.println(rtc.getSeconds());
 }
